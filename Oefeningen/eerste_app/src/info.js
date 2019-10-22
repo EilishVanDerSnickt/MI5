7f2eb8030db72dc18176020e21b49baa53819495
Eilish Van Der Snickt <eilish.vandersnickt@student.odisee.be>
2019-10-22 10:26:23 +0200

    commentaar toevoegen

--- Oefeningen/eerste_app/src/info.js
+++ Oefeningen/eerste_app/src/info.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 class Info extends React.Component {
+    //initialiseer constructor en zet de state gelijk aan een array van items en een boolean isLoaded
     constructor(props){
         super(props);
         this.state = {
@@ -10,6 +11,7 @@ class Info extends React.Component {
     } //constructor
 
     componentDidMount(){
+        //fecth de juiste url en vul de gegevens in de array en zet de isLoaded bool op true
         fetch('https://kitsu.io/api/edge/anime?filter[text]=one%20piece')
         .then(res => res.json())
         .then(json => {
@@ -21,10 +23,13 @@ class Info extends React.Component {
     } //componentDidMount
 
     render(){
+        // zet 2 variabelen gelijk aan de staat
         var {isLoaded, items} = this.state;
+        //wanneer de staat niet geladen is, toon Loading
         if (!isLoaded){
             return <h1>Loading ... </h1>
         } else {
+            //wanneer de staat wel geladen is maak variabelen aan met de geladen info die je nodig hebt
             var title = items.data[0].attributes.titles.en;
             var japanesetitle = items.data[0].attributes.titles.ja_jp;
             var startDate = items.data[0].attributes.startDate;
@@ -36,7 +41,7 @@ class Info extends React.Component {
             if (status == "current"){
                 status = "ongoing";
             }
-
+            //toon de variabelen
             return (
                 <div>
                    <h1>{title} - {japanesetitle}</h1>
